72d58ed48812cb500b8e210e2ffade1d00e83b12
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2022-11-06 18:14:26 -0500

    FireBase indexing
    Signed-off-by: RunTheBot <58890327+RunTheBot@users.noreply.github.com>

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,15 +9,15 @@ import {addDoc} from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: 'api-key',
-    authDomain: 'project-id.firebaseapp.com',
-    databaseURL: 'https://project-id.firebaseio.com',
-    projectId: 'project-id',
-    storageBucket: 'project-id.appspot.com',
-    messagingSenderId: 'sender-id',
-    appId: 'app-id',
-    measurementId: 'G-measurement-id',
+    apiKey: "AIzaSyALVYJaT3Q-tWD_j2VVDTI2o-JS0g_ah-Y",
+    authDomain: "volun-track.firebaseapp.com",
+    projectId: "volun-track",
+    storageBucket: "volun-track.appspot.com",
+    messagingSenderId: "348051927128",
+    appId: "1:348051927128:web:30169cf42d5053de5c1a2d",
+    measurementId: "G-WDQ5PPH1ZJ"
 };
+
 
 const app = initializeApp(firebaseConfig);
 // For more information on how to access Firebase in your project,
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,41 +3,48 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
+import {withNavigation} from "react-navigation";
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import {app} from "../../firebaseConfig"
 
-const SearchScreen = () => {
+const db = getFirestore(app);
+
+const SearchScreen = ({navigation}) => {
   const [term, setTerm] = useState('');
-  const [searchApi, results, errorMessage] = useResults();
+  const [errorMessage] = useResults();
+  const [results, setResults] = useResults();
+    const [keywords, setKeywords] = React.useState('')
 
-  const filterResultsByPrice = price => {
-    // price === '$' || '$$' || '$$$'
-    return results.filter(result => {
-      return result.price === price;
-    });
-  };
+  async function search(term){
+      const listingsCol = collection(db, 'listings')
+      const QTitle = query(listingsCol, where("title", "==", term));
+      const QDescription = query(listingsCol, where("description", "==", term));
+
+      const QTitleSnapshot = await getDocs(QTitle);
+      const QDescriptionSnapshot = await getDocs(QDescription);
+
+      const q = QTitleSnapshot.concat(QDescriptionSnapshot)
+
+      setResults(q)
+
+      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+      navigation.navigate("Results")
+  }
 
   return (
     <>
       <SearchBar
-        term={term}
-        onTermChange={setTerm}
-        onTermSubmit={() => searchApi(term)}
+          placeholder="Search Products ..."
+          onChangeText={(val) => { setKeywords(val) }}
+          onSubmitEditing={()=>console.log(`User typed ${keywords}`)}
+          value={keywords}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <ScrollView>
-        <ResultsList
-          results={filterResultsByPrice('$')}
-          title="Cost Effective"
-        />
-        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-        <ResultsList
-          results={filterResultsByPrice('$$$')}
-          title="Big Spender"
-        />
-      </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({});
 
-export default SearchScreen;
+export default withNavigation(SearchScreen);
