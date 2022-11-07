8cdad5674b6ddd106cecce410892cd05ccec6f8d
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2022-11-06 19:18:41 -0500

    Fix api error
    Signed-off-by: RunTheBot <58890327+RunTheBot@users.noreply.github.com>

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 import CreateButton from '../components/CreateButton';
+import useResults from "../hooks/useResults";
+import {collection, getDocs, query, where} from "firebase/firestore";
+import {app} from "../../firebaseConfig"
+
+const db = getFirestore(app);
 
 const HomeScreen = () => {
+
+    const [results, setResults] = useResults();
+    const [keywords, setKeywords] = React.useState('')
+
+    async function search(term){
+        const listingsCol = collection(db, 'listings')
+        const QTitle = query(listingsCol, where("title", "==", term));
+        const QDescription = query(listingsCol, where("description", "==", term));
+
+        const QTitleSnapshot = await getDocs(QTitle);
+        const QDescriptionSnapshot = await getDocs(QDescription);
+
+        const q = QTitleSnapshot.concat(QDescriptionSnapshot)
+
+        setResults(q)
+
+        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+        navigation.navigate("Results")
+    }
+
     return <View>
-        <SearchBar />
+        <SearchBar
+            placeholder="Search Products ..."
+            onChangeText={(val) => { setKeywords(val) }}
+            onSubmitEditing={()=>console.log(`User typed ${keywords}`)}
+            value={keywords}
+        />
         <Text style = {styles.topText}>It looks like you haven't added any</Text>
         <Text style = {styles.bottomText}>Volunteering tasks yet...</Text>
         <CreateButton title="Create a New Task!"/>
@@ -14,20 +45,20 @@ const HomeScreen = () => {
 }
 
 const styles =  StyleSheet.create({
-    topText: { 
+    topText: {
         fontSize: 15,
         alignSelf: 'center',
         marginTop: 100,
         color: '#1E2022'
     },
-    bottomText: { 
+    bottomText: {
         fontSize: 15,
         alignSelf: 'center',
         marginTop: 5,
         marginBottom: 120,
         color: '#1E2022'
     },
-   
+
 });
 
 export default HomeScreen;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -10,27 +10,7 @@ import {app} from "../../firebaseConfig"
 const db = getFirestore(app);
 
 const SearchScreen = ({navigation}) => {
-  const [term, setTerm] = useState('');
-  const [errorMessage] = useResults();
-  const [results, setResults] = useResults();
-    const [keywords, setKeywords] = React.useState('')
 
-  async function search(term){
-      const listingsCol = collection(db, 'listings')
-      const QTitle = query(listingsCol, where("title", "==", term));
-      const QDescription = query(listingsCol, where("description", "==", term));
-
-      const QTitleSnapshot = await getDocs(QTitle);
-      const QDescriptionSnapshot = await getDocs(QDescription);
-
-      const q = QTitleSnapshot.concat(QDescriptionSnapshot)
-
-      setResults(q)
-
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-
-      navigation.navigate("Results")
-  }
 
   return (
     <>
