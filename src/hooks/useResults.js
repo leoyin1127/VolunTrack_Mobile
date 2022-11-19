477ba998d1c5c960a901b64a8653866031259f01
andrewJ43 <112353503+andrewJ43@users.noreply.github.com>
2022-11-19 18:06:45 -0500

    Testing search functionality

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,11 +1,24 @@
 import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
 
 export default () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async searchTerm => {
-    console.log('Hi there!');
+    console.log('Starting search!');
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: 'volunteer',
+          location: 'toronto'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log('Something went wrong...');
+    }
   };
 
   // Call searchApi when component
@@ -17,3 +30,4 @@ export default () => {
 
   return [searchApi, results, errorMessage];
 };
+
