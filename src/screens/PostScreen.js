ffd15fce818e01fc580ed6a508a8fefc11f04757
Leo Yin <71328966+leoyin1127@users.noreply.github.com>
2022-11-20 18:20:39 -0500

    Update PostScreen.js

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -3,10 +3,10 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 
 const PostScreen = ({navigation}) => {
     return <View style = {styles.container}>
-        <Text>Post Screen!</Text>
+        <Text>Post Screen</Text>
         <Button
           title = "Click Here!"
-          onPress = {() => alert('Button Clicked!')}
+          onPress = {() => alert('Coming soon!')}
       />
     </View>
 }
