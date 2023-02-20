971fb02140aa5b5a2db2a9da06e341f256e5b0d0
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-02-19 19:58:01 -0500

    fixed spam
    Signed-off-by: RunTheBot <58890327+RunTheBot@users.noreply.github.com>

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses",
+  baseURL: "https://cors.zimjs.com/https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
       "Bearer J8IlQ6CHenk7D59VQbZcqDqiLkhXq0dPFkyiihu2ASMHCl8QlffPUi3jSN0eTOs2rIbZx7S4ajh1yneMnzmQwGOit5Tp26yL9JBMjFXdAJ5fzczET3CDuZhP7zV5Y3Yx",
