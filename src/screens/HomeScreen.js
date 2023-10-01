63f0fede876ae60d72a0d3f89e7bea0b7182b401
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-10-01 13:54:27 -0400

    Todolist on home screen
    Signed-off-by: RunTheBot <58890327+RunTheBot@users.noreply.github.com>

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,10 +5,35 @@ import SearchBar from '../components/SearchBar';
 import CreateButton from '../components/CreateButton';
 import {withNavigation} from "react-navigation";
 import useResults from "../hooks/useResults";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import TodoList from "./TodoList";
 
 const HomeScreen = ({navigation}) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
+    let [tasks, setTasks] = useState([]);
+    
+    const storeTask = async (value) => {
+        try {
+            await AsyncStorage.setItem("Todos", JSON.stringify(value));
+            console.log(JSON.parse(await AsyncStorage.getItem("Todos")));
+            
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    
+    const message = () => {
+        return (
+            <View>
+                <Text style = {styles.topText}>It looks like you haven't added any</Text>
+                <Text style = {styles.bottomText}>Volunteering tasks yet...</Text>
+                <CreateButton title="Create a New Task!"/>
+            </View>
+        )
+    }
+    
     return <View>
         <SearchBar
             term={searchTerm}
@@ -17,9 +42,9 @@ const HomeScreen = ({navigation}) => {
                 navigation.navigate('Menu', searchTerm);
             }}
         />
-        <Text style = {styles.topText}>It looks like you haven't added any</Text>
-        <Text style = {styles.bottomText}>Volunteering tasks yet...</Text>
-        <CreateButton title="Create a New Task!"/>
+        {
+            tasks.length > 0 ? <TodoList/> :  message()
+        }
     </View>
 }
 
