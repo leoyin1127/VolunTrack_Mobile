6b37794a87858d2549621f9720923a93d54368b1
Leo Yin <71328966+leoyin1127@users.noreply.github.com>
2023-11-19 16:09:43 -0500

    Revert "configure packages"
    This reverts commit f7939066ed6648639f68c7ab64d90455503f4a7a.

--- src/screens/HomepageScreen.js
+++ src/screens/HomepageScreen.js
@@ -4,7 +4,6 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import colors from '../../assets/colors/colors';
 import HomepageSearchBar from '../components/HomepageSearchBar';
-import bottomnavigator from '../components/BottomNavigator'
 
 const HomepageScreen = ({navigation}) => {
     return(
