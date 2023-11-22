7e2035b1063ff989a19b9561c6ecef059516aac7
kfcmmm123 <149546180+kfcmmm123@users.noreply.github.com>
2023-11-21 22:24:29 -0500

    Update App.js
    ok

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 
+// Import your screen components
 import AboutUsScreen from './src/screens/AboutUsScreen';
 import BookmarkedScreen from './src/screens/BookmarkedScreen';
 import HomepageScreen from './src/screens/HomepageScreen';
@@ -8,95 +10,83 @@ import MailScreen from './src/screens/MailScreen';
 import SearchScreen from './src/screens/SearchScreen'; 
 import VolunteeringScreen from './src/screens/VolunteeringScreen'; 
 
+// Import colors and vector icons
 import colors from './assets/colors/colors';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+// Define a Stack Navigator for the Mail tab
+const MailStack = createNativeStackNavigator();
+
+const MailStackScreen = () => {
+  return (
+    <MailStack.Navigator>
+      <MailStack.Screen name="Mail" component={MailScreen} />
+      <MailStack.Screen name="IndividualMail" component={IndividualMailScreen} />
+      {/* Add other screens in the Mail stack here */}
+    </MailStack.Navigator>
+  );
+};
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions = {{
-      style: styles.tabBar,
-      activeTintColor: colors.primary,
-      inactiveTintColor: colors.secondary,
-      headerShown: false, 
-
-      'tabBarShowLabel': false,
-      'tabBarStyle': [
-        {
-          'display': 'flex'
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarStyle: styles.tabBar,
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.secondary,
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let icon;
+          switch (route.name) {
+            case 'Homepage':
+              icon = focused ? require('./assets/icons/home.png') : require('./assets/icons/home.png');
+              break;
+            case 'Search':
+              icon = focused ? require('./assets/icons/loupe.png') : require('./assets/icons/loupe.png');
+              break;
+            case 'Mail':
+              icon = focused ? require('./assets/icons/email.png') : require('./assets/icons/email.png');
+              break;
+            case 'Bookmarked':
+              icon = focused ? require('./assets/icons/bookmark.png') : require('./assets/icons/bookmark.png');
+              break;
+            // Add other cases here for additional tabs
+          }
+          return <Image source={icon} style={{ width: size, height: size, tintColor: color }} />;
         },
-        null
-      ]
-    }}>
-      <Tab.Screen name = 'Homepage' component = {HomepageScreen} options = {{
-        tabBarIcon: ({focused}) => (<View style = {{alignItems: 'center', justifyContent: 'center', top: 10}}>
-          <Image source = {require('./assets/icons/home.png')} resizeMode = 'contain' style = {{
-            width: 32,
-            height: 32,
-            tintColor: focused ? colors.primary : colors.secondary
-          }}/>
-        </View>
-        )
-      }}/>
-      <Tab.Screen name = 'Search' component = {SearchScreen} options = {{
-        tabBarIcon: ({focused}) => (<View style = {{alignItems: 'center', justifyContent: 'center', top: 10}}>
-          <Image source = {require('./assets/icons/loupe.png')} resizeMode = 'contain' style = {{
-            width: 32,
-            height: 32,
-            tintColor: focused ? colors.primary : colors.secondary
-          }}/>
-        </View>
-        )
-      }}/>
-      <Tab.Screen name = 'Mail' component = {MailScreen} options = {{
-        tabBarIcon: ({focused}) => (<View style = {{alignItems: 'center', justifyContent: 'center', top: 10}}>
-          <Image source = {require('./assets/icons/email.png')} resizeMode = 'contain' style = {{
-            width: 32,
-            height: 32,
-            tintColor: focused ? colors.primary : colors.secondary
-          }}/>
-        </View>
-        )
-      }}/>
-      <Tab.Screen name = 'Bookmarked' component = {BookmarkedScreen} options = {{
-        tabBarIcon: ({focused}) => (<View style = {{alignItems: 'center', justifyContent: 'center', top: 10}}>
-          <Image source = {require('./assets/icons/bookmark.png')} resizeMode = 'contain' style = {{
-            width: 32,
-            height: 32,
-            tintColor: focused ? colors.primary : colors.secondary
-          }}/>
-        </View>
-        )
-      }}/>
+      })}
+    >
+      <Tab.Screen name='Homepage' component={HomepageScreen} />
+      <Tab.Screen name='Search' component={SearchScreen} />
+      <Tab.Screen name="Mail" component={MailStackScreen} /*options={{ tabBarBadge: 4 }*/ />
+      <Tab.Screen name='Bookmarked' component={BookmarkedScreen} />
+      {/* Add other Tab.Screen components here */}
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-const App = () => {
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = 'TabNavigator' component = {TabNavigator} options = {{headerShown: false}}/>
-        <Stack.Screen name = 'AboutUs' component = {AboutUsScreen} options = {{headerShown: false}}/>
-        <Stack.Screen name = 'IndividualMail' component = {IndividualMailScreen} options = {{headerShown: false}}/>
-        <Stack.Screen name = 'Volunteering' component = {VolunteeringScreen} options = {{headerShown: false}}/>
-      </Stack.Navigator>
+      <TabNavigator />
     </NavigationContainer>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   tabBar: {
-    bordertopLeftRadius: 20,
-    bordertopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: '#FFF',
+    paddingBottom: 5,
   }
-})
-
-export default App;
-
+});
